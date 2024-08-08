ad45fe1cdd8a0fce9a36cee91b96ada26b3778e9
Cole Rottenberg <cole.rottenberg@gmail.com>
2024-08-08 01:04:19 -0400

    before anyting breaks

--- algo_trade/data_engine/future.py
+++ algo_trade/data_engine/future.py
@@ -1,9 +1,10 @@
 import asyncio
 import os
-from typing import Any, Dict, Tuple, List
+from typing import Any, Dict, Tuple, List, Literal, Optional
 from abc import abstractmethod, ABC
 from enum import StrEnum, Enum
 from pathlib import Path
+from dataclasses import dataclass
 
 import aiohttp
 import databento as db
@@ -22,59 +23,64 @@ config: Dict[str, Any] = toml.load(config_path)
 class CATALOG(StrEnum):
     DATABENTO = f"data/catalog/databento/"
 
+class ASSET(StrEnum):
+    FUT = "FUT"
+    OPT = "OPT"
+    EQ = "EQ"
+
 # TODO: Add more datasets
 class DATASET(StrEnum):
     CME = "GLBX.MDP3"
 
-class Aggregation(StrEnum):
+
+# TODO: Add more schemas
+class Agg(StrEnum):
     DAILY = "ohlcv-1d"
     HOURLY = "ohlcv-1h"
     MINUTE = "ohlcv-1m"
     SECOND = "ohlcv-1s"
-
-# TODO: Add more schemas
-class Schema(Enum):
-    BAR = Aggregation
-
-class Data(ABC):
-    """
-    Data class to act as a base class for all data classes
+    def __str__(self):
+        return self.value
     
-    Attributes:
-    schema: Schema - The schema of the data
+    def __repr__(self):
+        return self.value
+
+class RollType(StrEnum):
+    CALENDAR = "c"
+    OPEN_INTEREST = "n"
+    VOLUME = "v"
+    def __str__(self):
+        return self.value
     
-    Methods:
-    get_schema() -> Schema - Returns the schema of the data
-    """
+    def __repr__(self):
+        return self.value
 
-    def __init__(self, schema: Schema):
-        self.schema: Schema = schema
+class ContractType(StrEnum):
+    FRONT = "0"
+    BACK = "1"
+    def __str__(self):
+        return self.value
     
-    def get_schema(self) -> Schema:
-        """
-        Returns the schema of the data
-        
-        Args:
-        None
-        
-        Returns:
-        Schema: The schema of the data
-        """
-        return self.schema
+    def __repr__(self):
+        return self.value
 
 
-class Bar(Data):
+class Bar():
     """
     Bar class to act as a base class for all bar classes
     
     Attributes:
+    -   instrument_id: str - The instrument_id of the bar
+    -   schema: Schema.BAR - The schema of the bar
+    -   catalog: CATALOG - The catalog location of existing instrument data
+    -   data: db.DBNStore - The data of the bar
+    -   definitions: db.DBNStore - The definitions of the bar
     -   timestamp: pd.Timestamp - The timestamp of the bar
     -   open: pd.Series - The open price of the bar
     -   high: pd.Series - The high price of the bar
     -   low: pd.Series - The low price of the bar
     -   close: pd.Series - The close price of the bar
     -   volume: pd.Series - The volume of the bar
-    -   schema: Schema.BAR - The schema of the bar
 
     Methods:
 
@@ -91,17 +97,73 @@ class Bar(Data):
     CONSTRUCTORS:
     -   construct() -> None - Constructs the bar by first attempting to retrieve the data and definitions from the data catalog
     """
-    def __init__(self, schema: Schema.BAR, catalog: CATALOG = CATALOG.DATABENTO):
-        super.__init__(schema)
-        self.data: db.DBNStore = None
-        self.definitions: db.DBNStore = None
-        self.timestamp: pd.Series = None
-        self.open: pd.Series = None
-        self.high: pd.Series = None
-        self.low: pd.Series = None
-        self.close: pd.Series = None
-        self.volume: pd.Series = None
-        self.schema: Schema.BAR = schema
+    def __init__(self, instrument_id: str, dataset: DATASET, schema: Agg, catalog: CATALOG = CATALOG.DATABENTO):
+        self.data: pd.DataFrame
+        self.definitions: pd.DataFrame
+        self.timestamp: pd.Series
+        self.open: pd.Series
+        self.high: pd.Series
+        self.low: pd.Series
+        self.close: pd.Series
+        self.volume: pd.Series
+        self.instrument_id: str = instrument_id
+        self.dataset: DATASET = dataset
+        self.schema: Agg = schema
+        self.catalog: CATALOG = catalog
+
+    def __str__(self) -> str:
+        return f"Bar: {self.instrument_id} - {self.dataset} - {self.schema}"
+
+    def __repr__(self) -> str:
+        return f"Bar: {self.instrument_id} - {self.dataset} - {self.schema}"
+
+    def get_instrument_id(self) -> str:
+        """
+        Returns the instrument_id of the bar
+        
+        Args:
+        None
+        
+        Returns:
+        str: The instrument_id of the bar
+        """
+        return self.instrument_id
+
+    def get_dataset(self) -> DATASET:
+        """
+        Returns the dataset of the bar
+        
+        Args:
+        None
+        
+        Returns:
+        DATASET: The dataset of the bar
+        """
+        return self.dataset
+
+    def get_schema(self) -> Agg:
+        """
+        Returns the schema of the bar
+        
+        Args:
+        None
+        
+        Returns:
+        Schema.BAR: The schema of the bar
+        """
+        return self.schema
+
+    def get_catalog(self) -> CATALOG:
+        """
+        Returns the catalog location of the existing instrument data
+        
+        Args:
+        None
+        
+        Returns:
+        CATALOG: The catalog location of the existing instrument data
+        """
+        return self.catalog
 
     def get_timestamp(self) -> pd.Series:
         """
@@ -205,9 +267,13 @@ class Bar(Data):
         pd.DataFrame: The backadjusted bar as a dataframe
         """
         # TODO: Implement backadjustment
+        if self.definitions or self.data is None:
+            raise ValueError("Data and Definitions are not present")
+
+        # Backadjust the bar
         pass
 
-    def construct(self) -> None:
+    def construct(self, client: db.Historical, roll_type: RollType, contract_type: ContractType, range: dict[str, str]) -> None:
         """
         Constructs the bar by first attempting to retrieve the data and definitions from the data catalog
         
@@ -219,8 +285,74 @@ class Bar(Data):
         """
         # TODO: Implement construct method
 
+        data_path: Path = Path(f"{self.catalog}/{self.instrument_id}/{self.schema}/{roll_type}-{contract_type}-data.parquet")
+        definitions_path: Path = Path(f"{self.catalog}/{self.instrument_id}/{self.schema}/{roll_type}-{contract_type}-definitions.parquet")
 
-        pass
+        if data_path.exists() and definitions_path.exists():
+            try:
+                self.data = pd.read_parquet(data_path)
+                self.definitions = pd.read_parquet(definitions_path)
+            except Exception as e:
+                print(f"Error: {e}")
+                return
+
+            start: pd.Timestamp = pd.Timestamp(range[self.dataset]["start"])
+            end: pd.Timestamp = pd.Timestamp(range[self.dataset]["end"])
+
+            data_end: pd.Timestamp = self.data["Timestamp"].iloc[-1]
+            definitions_end: pd.Timestamp = self.definitions["Timestamp"].iloc[-1]
+
+            # Check if the data and definitions are up to date
+
+            if data_end != end or definitions_end != end:
+                print("Data and Definitions are not up to date for {self.instrument_id}")
+                symbols: str = f"{self.instrument_id}.{roll_type}.{contract_type}"
+                new_data: db.DBNStore = client.timeseries.get_range(dataset=self.dataset, symbols=symbols, schema=db.Schema.from_str(self.schema), start=data_end, end=end)
+                new_definitions: db.DBNStore = new_data.request_full_definitions(client=client)
+
+                # Combine new data with existing data and skip duplicates if they exist
+                self.data = pd.concat([self.data, new_data.to_df()]).drop_duplicates(subset=["Timestamp"], keep="last")
+                self.definitions = pd.concat([self.definitions, new_definitions.to_df()]).drop_duplicates(subset=["Timestamp"], keep="last")
+
+                # Save the new data and definitions to the catalog
+                self.data.to_parquet(data_path)
+                self.definitions.to_parquet(definitions_path)
+
+                # Set the timestamp, open, high, low, close, and volume
+                self.timestamp = self.data["Timestamp"]
+                self.open = self.data["Open"]
+                self.high = self.data["High"]
+                self.low = self.data["Low"]
+                self.close = self.data["Close"]
+                self.volume = self.data["Volume"]
+
+        else:
+            print(f"Data and Definitions not present for {self.instrument_id}")
+            # Submit a job request to retrieve the data and definitions
+            symbols: str = f"{self.instrument_id}.{roll_type}.{contract_type}"
+            # TODO: Implement job request submission
+            # details: dict[str, Any] = client.batch.submit_job(dataset=self.dataset, symbols=symbols, schema=db.Schema.from_str(self.schema), encoding=db.Encoding.DBN start=start, end=end, stype_in=db.SType.CONTINUOUS, split_duration=db.SplitDuration.NONE)
+            # print(f"Job Request Submitted: {details["symbols"]} - {details["schema"]} - {details["start"]} - {details["end"]}")
+            data: db.DBNStore = client.timeseries.get_range(dataset=self.dataset, symbols=symbols, schema=db.Schema.from_str(self.schema), start=start, end=end)
+            definitions: db.DBNStore = data.request_full_definitions(client=client)
+
+            # Save the data and definitions to the catalog
+            data.to_parquet(data_path)
+            definitions.to_parquet(definitions_path)
+
+            self.data = data.to_df()
+            self.definitions = definitions.to_df()
+
+            # Set the timestamp, open, high, low, close, and volume
+            self.timestamp = self.data["Timestamp"]
+            self.open = self.data["Open"]
+            self.high = self.data["High"]
+            self.low = self.data["Low"]
+            self.close = self.data["Close"]
+            self.volume = self.data["Volume"]
+
+            # WARNING: The API "should" be able to handle data requests under 5 GB but have had issues in the pass with large requests
+            return
 
 class Instrument(ABC):
     """
@@ -235,23 +367,15 @@ class Instrument(ABC):
     get_dataset() -> str - Returns the dataset of the instrument
     get_collection() -> Tuple[str, str] - Returns the symbol and dataset of the instrument
 
-    construct() -> None - Constructs the instrument by first attempting to retrieve the data and definitions from the data catalog
-    If the data and definitions are not present, then the instrument will submit a job request to retrieve the data and definitions as a download job
-    If the data and definitions are present, then the instrument will open the DBZ files and store them within the instrument object. The instrument will then check the available data and definitions through a certain databento API endpoint.
-    If the last entry of the data and definitions are not the same, then the instrument will call another API to get the range of the data and defintions and then merge them together.
+    The instrument class is an 
 
-    The instrument class is the base class for all asset classes. It contains the symbol and dataset of the instrument
-    The data of each instrument is stored within a data catalog with the corresponding aggregate data and definitions data.
-
-    When initializing an instrument, the symbol and dataset are passed in as arguments.
     """
     def __init__(self, symbol: str, dataset: str):
         self.symbol = symbol
         self.dataset = dataset
         self.client: db.Historical = db.Historical(config["databento"]["api_historical"])
-        self.data: db.DBNStore = None
-        self.definitions: db.DBNStore = None
-        self.raw = None
+        self.asset: Optional[ASSET] = None
+        
 
     def get_symbol(self) -> str:
         """
@@ -288,25 +412,6 @@ class Instrument(ABC):
         Tuple[str, str]: The symbol and dataset of the instrument
         """
         return (self.symbol, self.dataset)
-    
-    @abstractmethod
-    def construct(self) -> None:
-        """
-        Constructs the instrument by first attempting to retrieve the data and definitions from the data catalog
-
-        If the data and definitions are not present, then the instrument will submit a job request to retrieve the data and definitions as a download job
-
-        If the data and definitions are present, then the instrument will open the DBZ files and store them within the instrument object. The instrument will then check the available data and definitions through a certain databento API endpoint.
-
-        This is a placeholder function that will be implemented in the future
-
-        Args:
-        None
-
-        Returns:
-        None 
-        """
-        pass 
 
 class Future(Instrument):
     """
