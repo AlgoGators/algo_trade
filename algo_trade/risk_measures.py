fdd9757bf55206530dea9fa74b547aaf9de3fd93
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2024-08-26 19:06:01 -0400

    back to inheritance

--- algo_trade/risk_measures.py
+++ algo_trade/risk_measures.py
@@ -27,7 +27,7 @@ class _utils:
 class StandardDeviation(pd.DataFrame):
     def __init__(self, data : pd.DataFrame = None) -> None:
         super().__init__(data)
-        self.__is_annualized = False
+        self.__is_annualized : bool = False
 
     def annualize(self, inplace=False) -> Optional[Self]:
         if self.__is_annualized:
@@ -46,6 +46,25 @@ class StandardDeviation(pd.DataFrame):
 
     def to_variance(self) -> 'Variance':
         return Variance(self ** 2)
+
+    # def __mul__(self, other : float) -> 'StandardDeviation':
+    #     if isinstance(other, pd.DataFrame):
+    #         return self.__data * other
+    #     if isinstance(other, pd.Series):
+    #         return self.__data * other
+    #     # return StandardDeviation(self.__data * other)
+    
+    # def __pow__(self, other : float) -> 'StandardDeviation':
+    #     return self.__data ** other
+
+    # def __rtruediv__(self, other : float) -> 'StandardDeviation':
+    #     return other / self.__data
+    
+    # def __truediv__(self, other : float) -> 'StandardDeviation':
+    #     return self.__data / other
+    
+    # def __getitem__(self, key : str) -> pd.Series:
+    #     return self.__data[key]
 
 class Variance(pd.DataFrame):
     def __init__(self, data : pd.DataFrame = None) -> None:
