296507ebf33c0a26e61f7845056bc2124d397553
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2024-09-14 10:33:12 -0400

    update_account to make necessary trades

---
+++ algo_trade/update_account.py
@@ -0,0 +1,24 @@
+import ipaddress
+
+from decimal import Decimal
+
+from algo_trade.ib_utils.account import Account, Position, Contract
+from algo_trade.ib_utils.api_handler import api_handler_context
+from algo_trade.ib_utils.trading_algorithm import TradingAlgorithm
+from algo_trade.ib_utils._enums import AdaptiveOrderPriority
+from algo_trade.ib_utils._config import LOCALHOST, PORT, CLIENT_ID
+
+def update_account(account : Account, order_priority : AdaptiveOrderPriority):
+    with api_handler_context(ipaddress.ip_address(LOCALHOST), PORT, CLIENT_ID) as api_handler:
+        api_handler.cancel_outstanding_orders()
+
+        held_account = Account(api_handler.current_positions)
+
+        api_handler.initialize_desired_contracts(account, 5)
+
+        trades = account - held_account
+
+        api_handler.place_orders(
+            trades=trades,
+            trading_algorithm=TradingAlgorithm(order_priority).adaptive_market_order
+        )
