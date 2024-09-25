9b413de4c99323553b67e0cead8708188d326cab
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2024-09-25 16:58:34 -0400

    typehint

--- algo_trade/_utils.py
+++ algo_trade/_utils.py
@@ -1,15 +1,18 @@
 import warnings
 import functools
-from typing import Callable
+from typing import Callable, Tuple, TypeVar, Any, Dict
 
-def deprecated(func) -> Callable:
+# Define a generic type for the return value of the function
+R = TypeVar('R')
+
+def deprecated(func: Callable[..., R]) -> Callable[..., R]:
     """This is a decorator which can be used to mark functions
     as deprecated. It will result in a warning being emitted
     when the function is used."""
     @functools.wraps(func)
-    def new_func(*args, **kwargs) -> Callable:
+    def new_func(*args: Tuple[Any], **kwargs: Dict[str, Any]) -> R:
         warnings.simplefilter('always', DeprecationWarning)  # turn off filter
-        warnings.warn("Call to deprecated function {}.".format(func.__name__),
+        warnings.warn(f"Call to deprecated function {func.__name__}.",
                       category=DeprecationWarning,
                       stacklevel=2)
         warnings.simplefilter('default', DeprecationWarning)  # reset filter
